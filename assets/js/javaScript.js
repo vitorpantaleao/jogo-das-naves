a964f12289820bf557b0d0748a4960f3f6956faa
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-28 23:22:58 -0300

    adiciona colisao amigo com inimigo 2

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -191,6 +191,16 @@ function start() {
             reposicionaAmigo();
             $("#amigo").remove();
         }
+
+        // Inimigo 2 com o amigo
+        if (colisao6.length > 0) {
+            amigoX = parseInt($("#amigo").css("left"));
+            amigoY = parseInt($("#amigo").css("top"));
+            explosao3(amigoX,amigoY);
+
+            $("#amigo").remove();
+            reposicionaAmigo();
+        }
         
     }
 
@@ -228,6 +238,19 @@ function start() {
             div2.remove();
             window.clearInterval(tempoExplosao2);
             tempoExplosao2 = null;
+        }
+    }
+
+    // Explosao 3
+    function explosao3(amigoX,amigoY) {
+        $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+        $("#explosao3").css("top", amigoY);
+        $("#explosao3").css("left", amigoX);
+        var tempoExplosao3 = window.setInterval(resetaExplosao3, 1000);
+        function resetaExplosao3() {
+            $("#explosao3").remove();
+            window.clearInterval(tempoExplosao3);
+            tempoExplosao3 = null;
         }
     }
 
