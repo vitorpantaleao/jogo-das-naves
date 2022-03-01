50e05bafe8c09b5b0bb2ceb0fe08ca50db6d1897
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-28 22:36:53 -0300

    adiciona movimento amigo

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -33,6 +33,7 @@ function start() {
         movejogador()
         moveinimigo1()
         moveinimigo2()
+        moveamigo()
 	}
 
     //Funcao que movimenta o fundo do jogo
@@ -87,6 +88,15 @@ function start() {
         $("#inimigo2").css("left",posicaoX - 3);
         if (posicaoX <= 0) {
             $("#inimigo2").css("left", 775);     
+        }
+    }
+
+    // Funcao move amigo
+    function moveamigo() {
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left",posicaoX + 1);
+        if (posicaoX > 906) {
+            $("#amigo").css("left", 0);  
         }
     }
 
