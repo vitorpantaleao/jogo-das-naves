382b373f7bcdeee507efefbcae07e2c5de5fbabc
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-28 22:24:13 -0300

    adiciona movimento no helicoptero utilizando o teclado

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -9,17 +9,54 @@ function start() {
 
 	//Game Loop
     var jogo = {}
+    var tecla = {
+        W: 87,
+        S: 83,
+        D: 68
+    }
+    jogo.pressionou = [];
 
-	jogo.timer = setInterval(loop, 30);
-	function loop() {
-	    movefundo();
+    //Verifica se o usuario pressionou alguma tecla	
+	$(document).keydown(function(e){
+        jogo.pressionou[e.which] = true;
+    });
+
+    $(document).keyup(function(e){
+        jogo.pressionou[e.which] = false;
+    });
+
+    jogo.timer = setInterval(loop, 30);
+    function loop() {
+        movefundo();
+        movejogador();
 	}
 
     //Funcao que movimenta o fundo do jogo
-	
 	function movefundo() {
         esquerda = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position",esquerda - 2);
     }
+
+    //Funcao que movimenta o jogador
+    function movejogador() {
+	
+        if (jogo.pressionou[tecla.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo - 10);
+        
+        }
+        
+        if (jogo.pressionou[tecla.S]) {
+            
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo + 10);	
+        }
+        
+        if (jogo.pressionou[tecla.D]) {
+            
+            //Chama funcao Disparo	
+        }
+    
+        }
 
 }
