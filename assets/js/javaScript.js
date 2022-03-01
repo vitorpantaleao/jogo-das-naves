5aacef30ccea01d677993aeb3e48add84ea3e221
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-28 23:11:13 -0300

    adiciona disparos contra os inimigos

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -159,7 +159,32 @@ function start() {
 
             $("#inimigo2").remove();
             reposicionaInimigo2();
-        }	
+        }
+
+        // Disparo com o inimigo 1
+        if (colisao3.length > 0) {
+            inimigo1X = parseInt($("#inimigo1").css("left"));
+            inimigo1Y = parseInt($("#inimigo1").css("top"));
+
+            explosao1(inimigo1X,inimigo1Y);
+            $("#disparo").css("left", 950);
+
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+        }
+
+        // Disparo com o inimigo2
+	    if (colisao4.length > 0) {
+            inimigo2X = parseInt($("#inimigo2").css("left"));
+            inimigo2Y = parseInt($("#inimigo2").css("top"));
+            $("#inimigo2").remove();
+
+            explosao2(inimigo2X,inimigo2Y);
+            $("#disparo").css("left", 950);
+
+            reposicionaInimigo2();
+        }
         
     }
 
@@ -191,12 +216,12 @@ function start() {
         div2.css("left", inimigo2X);
         div2.animate({width:200, opacity:0}, "slow");
         
-        var tempoExplosao2=window.setInterval(removeExplosao2, 1000);
+        var tempoExplosao2 = window.setInterval(removeExplosao2, 1000);
         
         function removeExplosao2() {
             div2.remove();
             window.clearInterval(tempoExplosao2);
-            tempoExplosao2=null;
+            tempoExplosao2 = null;
         }
     }
 
