0d78d7c6e8994e6d6e7367f05061fb257dd823ae
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-28 23:53:08 -0300

    adiciona game over e exibe placar

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -334,9 +334,28 @@ function start() {
 
         if (energiaAtual == 0) {
             $("#energia").css("background-image", "url(assets/img/energia0.png)");
-            //Game Over
+            gameOver()
         }
 
+    }
+
+    // Funcao GAME OVER
+	function gameOver() {
+        fimdejogo = true;
+        musica.pause();
+        somGameover.play();
+
+        window.clearInterval(jogo.timer);
+        jogo.timer = null;
+
+        $("#jogador").remove();
+        $("#inimigo1").remove();
+        $("#inimigo2").remove();
+        $("#amigo").remove();
+
+        $("#fundoGame").append("<div id='fim'></div>");
+
+        $("#fim").html("<h1> Game Over!! </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<a class='end' id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></a>");
     }
 
 } // Fim Função Start
