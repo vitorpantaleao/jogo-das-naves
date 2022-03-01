414e833318174026fa2eb5f0612e3cb3c2160e82
Vitor Pantaleão <vitor-hmp@hotmail.com>
2022-02-28 22:11:48 -0300

    adiciona animação inimigo

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -3,7 +3,7 @@ function start() {
 	$("#inicio").hide();
 	
 	$("#fundoGame").append("<div id='jogador' class='anima1'></div>");
-	$("#fundoGame").append("<div id='inimigo1'></div>");
+	$("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
 	$("#fundoGame").append("<div id='inimigo2'></div>");
 	$("#fundoGame").append("<div id='amigo'></div>");
 
